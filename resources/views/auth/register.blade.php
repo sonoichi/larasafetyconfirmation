83e925d0ac816d3b1d3182bc00fd12542c4f323b
sonoichi <handa.sonoichi@gmail.com>
2017-01-23 16:24:24 +0900

    + desined top

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top:2em">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -81,6 +81,7 @@
                                 </button>
                             </div>
                         </div>
+                        <div class="col-md-6 col-md-offset-6"><a href="../"><p>戻る</p></a></div>
                     </form>
                 </div>
             </div>
