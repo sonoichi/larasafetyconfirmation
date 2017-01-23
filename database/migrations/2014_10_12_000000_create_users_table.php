7a33890322743646ad45cc2365d1f365fcfaeaeb
sonoichi <handa.sonoichi@gmail.com>
2017-01-23 16:10:31 +0900

    + 2low

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->INTEGER('work_id')->unique();
+            $table->integer('work_id')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('tell');
