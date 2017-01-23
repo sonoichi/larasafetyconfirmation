2f5f86a676036408c2aef46dd593aa0a86e65265
sonoichi <handa.sonoichi@gmail.com>
2017-01-23 13:46:58 +0900

    comment update

--- app/Http/Controllers/EditController.php
+++ app/Http/Controllers/EditController.php
@@ -13,10 +13,18 @@ class EditController extends Controller
         //確認用 
         print_r($input);
         echo $_POST['safety'];
+
+        //Update
         DB::table('safe_info')
            ->where('work_id',$_POST['work_id'])
-           ->update(['safety' => $_POST['safety']]);
+           ->update(['safety' => $_POST['safety'], 'comment' => $_POST['comment']]);
 
-        return '送信されました';
+        //戻る用 'Users'
+        $users = DB::table('safe_info')
+          ->join('worker_list','safe_info.work_id', '=', 'worker_list.work_id')
+          ->whereNotIn('safe_info.safety', ['問題ない'])
+          ->get();
+
+        return view('charge/list',compact('users'));
     }
 }
